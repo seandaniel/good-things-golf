76a7363ba822a92d3d75e2e14abe72f7b8a4f993
seandaniel <degroot.sd@gmail.com>
2022-03-20 09:53:04 -0400

    Single styling

--- footer.php
+++ footer.php
@@ -4,12 +4,10 @@
     <h3>Images courtesy of Sdg</h3>
     <div class="footer-icon-container">
       <a href="<?php echo get_home_url() ?>" title="Home"><i class="fa-solid fa-house" aria-hidden="true"></i></i></a>
-      <a href="#top" title="Back to top"><i class="fa-solid fa-arrow-up" aria-hidden="true"></i></i></a>
+      <a href="#top" title="Back to top"><i class="fa-solid fa-circle-arrow-up" aria-hidden="true"></i></i></a>
     </div>
   </div>
 </footer>
-
-
 
 <?php wp_footer(); ?>
 </body>
--- single.php
+++ single.php
@@ -1,29 +1,27 @@
 <?php get_header(); ?>
 <div class="wrapper">
-  <section>
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+ 
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+  <section class="item-level-container" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <div class="entry-content">
-          <?php the_content(); ?>
-          <?php wp_link_pages(array(
-            'before' => '<div class="page-link"> Pages: ',
-            'after' => '</div>'
-          )); ?>
-        </div><!-- .entry-content -->
+      <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages(array(
+          'before' => '<div class="page-link"> Pages: ',
+          'after' => '</div>'
+        )); ?>
+      </div><!-- .entry-content -->
+      <h1><?php the_title(); ?></h1>
+      <h2><?php echo get_the_date(); ?></h2>
 
       <div class="home-container">
         <a href="<?php echo get_home_url(); ?>" title="Home"><i class="fa-solid fa-house" aria-hidden="true"></i></a>
       </div>
 
 
-    <?php endwhile; // end of the loop. ?>
+  <?php endwhile; // end of the loop. ?>
 
-  </div> <!-- /.content -->
-
-
-</section>
+</section> <!-- /.content -->
 
 <?php get_footer(); ?>
